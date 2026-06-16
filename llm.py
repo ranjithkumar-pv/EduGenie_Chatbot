8bec1051c4f580ed8da3b54dc0f765c5b85f7990
ranjith-7102 <rp0030@srmist.edu.in>
2026-06-16 18:21:18 +0530

    Refactor environment variable handling and update package versions in requirements

--- llm.py
+++ llm.py
@@ -17,10 +17,6 @@ from langchain_groq import ChatGroq
 from langchain_core.documents import Document
 
 load_dotenv()
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-if not GROQ_API_KEY:
-    raise ValueError("❌ Missing GROQ_API_KEY in .env")
 
 nltk.download("stopwords")
 nltk.download("punkt")
